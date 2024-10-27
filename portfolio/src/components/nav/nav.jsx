72e5e95a49f89ef9ac2e3921379b30fb36530c18
bodymostafa1 <139517580+bodymostafa1@users.noreply.github.com>
2024-10-27 16:33:19 -0700

    new cv

--- portfolio/src/components/nav/nav.jsx
+++ portfolio/src/components/nav/nav.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import myResume from "../Files/abderlahman mostafa cvv.pdf"
 import { Link } from 'react-router-dom';
 import './nav.css'
  function Nav() {
@@ -8,7 +7,7 @@ import './nav.css'
     <div className=' d-flex justify-content-center m-2 '>
     <Link className='m-3 px-4 text-decoration-none text-white' to={"/home"}><span className='purple-hover'>Home</span></Link>
     <Link className='m-3 px-4 text-decoration-none text-white' to={"/projects"}><span className='purple-hover'>Projects</span></Link>
-    <a href='/' download={"../Files/abderlahman mostafa cvv.pdf"} className='m-3 px-4 text-decoration-none text-white'><span className='purple-hover'>Resume</span></a>
+    <a href='/' download={"../Files/abderlahman mostafa cv.docx"} className='m-3 px-4 text-decoration-none text-white'><span className='purple-hover'>Resume</span></a>
     </div>
    </div>
   );
