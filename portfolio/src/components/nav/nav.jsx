1e323fdca8327cc79f6b0a0d2af2f449685d541d
bodymostafa1 <139517580+bodymostafa1@users.noreply.github.com>
2024-10-30 09:43:11 -0700

    file download

--- portfolio/src/components/nav/nav.jsx
+++ portfolio/src/components/nav/nav.jsx
@@ -7,7 +7,7 @@ import './nav.css'
     <div className=' d-flex justify-content-center m-2 '>
     <Link className='m-3 px-4 text-decoration-none text-white' to={"/home"}><span className='purple-hover'>Home</span></Link>
     <Link className='m-3 px-4 text-decoration-none text-white' to={"/projects"}><span className='purple-hover'>Projects</span></Link>
-    <a href='/Abdelrahman mostafa cv.docx' className='m-3 px-4 text-decoration-none text-white'><span className='purple-hover'>Resume</span></a>
+    <a href='_abdelrahman mostafa cv.docx' download className='m-3 px-4 text-decoration-none text-white'><span className='purple-hover'>Resume</span></a>
     </div>
    </div>
   );
